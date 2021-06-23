bf8745b3bc939584059ce9e89fa936742a0822c3
Tippin <richard.tippin@gmail.com>
2021-06-23 18:58:58 -0400

    migrating UI from messenger core to this package

---
+++ src/MessengerUiServiceProvider.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace RTippin\MessengerUi;
+
+use Illuminate\Support\ServiceProvider;
+
+class MessengerUiServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap any package services.
+     *
+     * @return void
+     */
+    public function boot(): void
+    {
+        //
+    }
+
+    /**
+     * Register any package services.
+     *
+     * @return void
+     */
+    public function register(): void
+    {
+        //
+    }
+}
